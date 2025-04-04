81e0b99afc0ab74562fbfff7bb1bd714d837b7c6
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-04-04 18:08:38 +0530

    Time: 0 ms (100%), Space: 22 MB (87.17%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    pair<TreeNode*, int> dfs(TreeNode* node) {
+        if (!node) return {nullptr, 0};
+        auto left = dfs(node->left);
+        auto right = dfs(node->right);
+        if (left.second > right.second) return {left.first, left.second + 1};
+        if (right.second > left.second) return {right.first, right.second + 1};
+        return {node, left.second + 1};
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return dfs(root).first;
+    }
+};
