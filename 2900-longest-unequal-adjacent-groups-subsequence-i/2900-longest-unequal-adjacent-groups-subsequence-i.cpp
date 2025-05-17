a7e7627125b09a07fc3d3eef7e52053bbc8a8eae
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-05-17 15:42:59 +0530

    Time: 2 ms (26.07%), Space: 30.1 MB (36.26%) - LeetHub

---
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        vector<string> result;
+        int n = words.size();
+        for (int i = 0; i < n; ++i) {
+            if (result.empty() || groups[i] != groups[i - 1]) {
+                result.push_back(words[i]);
+            }
+        }
+        return result;
+    }
+};
