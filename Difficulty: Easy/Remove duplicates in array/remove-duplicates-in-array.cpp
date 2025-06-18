2361152960fb2c860230b14ab62a291177536b9d
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-06-18 20:48:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates in array/remove-duplicates-in-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+       unordered_set<int> seen;
+    vector<int> result;
+
+    for (int num : arr) {
+        if (seen.find(num) == seen.end()) {
+            seen.insert(num);
+            result.push_back(num);
+        }
+    }
+    return result;
+    }
+};
