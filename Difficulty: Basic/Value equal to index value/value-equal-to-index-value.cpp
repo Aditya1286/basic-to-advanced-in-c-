77d4c52847d6396730e882b2c3e7e08ef720f4ed
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-06-19 20:13:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Value equal to index value/value-equal-to-index-value.cpp
@@ -0,0 +1,15 @@
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find elements in the array that are equal to their index.
+    vector<int> valueEqualToIndex(vector<int>& arr) {
+        vector<int> res;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==i+1){
+                res.push_back(arr[i]);
+            }
+        }
+        return res;
+    }
+};
