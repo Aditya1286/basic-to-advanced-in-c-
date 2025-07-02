fa8d7cac6bbc91f8d1d3caa3288cb955ec1b7d63
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-07-02 20:39:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort Elements by Decreasing Frequency/sort-elements-by-decreasing-frequency.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<int> sortByFreq(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+        sort(arr.begin(), arr.end(), [&](int a, int b) {
+            if (freq[a] == freq[b])
+                return a < b;
+            return freq[a] > freq[b];
+        });
+        return arr;
+    }
+};
