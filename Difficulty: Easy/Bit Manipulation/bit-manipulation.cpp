2a2d39291faa08b167ec047c902e6e5f52b6b64f
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-08-13 00:43:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bit Manipulation/bit-manipulation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void bitManipulation(int num, int i) {
+        int mask = 1 << (i - 1);
+
+        int getBit = (num & mask) ? 1 : 0;
+        int setBit = num | mask;
+        int clearBit = num & (~mask);
+
+        cout << getBit << " " << setBit << " " << clearBit;
+    }
+};
