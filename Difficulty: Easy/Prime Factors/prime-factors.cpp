755657e0fe8923b4aa8b360cb1adba005d3055f9
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-08-18 15:14:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Factors/prime-factors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> primeFac(int n) {
+        vector<int> res;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                res.push_back(i);
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+        if (n > 1) {
+            res.push_back(n);
+        }
+        return res;
+    }
+};
