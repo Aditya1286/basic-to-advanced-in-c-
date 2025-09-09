e36790639688fc304b8abae8fc22e8f82b411f31
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-09-09 14:36:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    // Function to check if we can reach the last index from 0th index.
+    bool canReach(vector<int> &arr) {
+        int maxIndex=0;
+        for(int i=0;i<arr.size();i++){
+            if(i>maxIndex)return false;
+            maxIndex=max(maxIndex,i+arr[i]);
+        }
+        return true;
+    }
+};
