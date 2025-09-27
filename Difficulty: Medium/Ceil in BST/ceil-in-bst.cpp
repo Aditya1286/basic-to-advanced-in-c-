59e89056e0e7f4c20239a52e64080ca81b5f28ef
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-09-27 16:27:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ceil in BST/ceil-in-bst.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int findCeil(Node* root, int key) {
+        int ceil=-1;
+        while(root){
+            if(root->data==key){
+                ceil=root->data;
+                return ceil;
+            }
+            if(key>root->data){
+                root=root->right;
+            }
+            else{
+                ceil=root->data;
+                root=root->left;
+            }
+        }
+        return ceil;
+    }
+};
