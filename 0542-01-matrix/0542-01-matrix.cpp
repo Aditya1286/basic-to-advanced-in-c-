3791e4f57b188d862f786e977d29ffd7d9b34770
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-10-11 09:13:53 +0530

    Time: 27 ms (30.9%), Space: 38.9 MB (22.56%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+
+        queue<pair<pair<int, int>, int>> q;
+
+        // Start BFS from all 0s
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    visited[i][j] = 1;
+                    q.push({{i, j}, 0});
+                }
+            }
+        }
+
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int dis = q.front().second;
+            q.pop();
+
+            ans[row][col] = dis;
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    visited[nrow][ncol] == 0) {
+                    visited[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, dis + 1});
+                }
+            }
+        }
+
+        return ans;
+    }
+};
