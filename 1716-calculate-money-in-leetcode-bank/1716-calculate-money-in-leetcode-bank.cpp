7ff2fe211944d5b3bd23eeaa7cae72bfe97ec022
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-10-25 18:49:02 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.15%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int weeks = n / 7;
+        int days = n % 7;
+        int total = 0;
+        total += 7 * weeks * (weeks + 1) / 2 + 21 * weeks;
+        int start = weeks + 1;
+        total += days * (2 * start + (days - 1)) / 2;
+        return total;
+    }
+};
