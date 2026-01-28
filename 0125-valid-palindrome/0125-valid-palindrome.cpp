39629780a8a181f8dba4399171f9a06cab8984e6
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-01-28 23:06:37 +0530

    Time: 1 ms (46.29%), Space: 10.8 MB (5.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,28 @@
 class Solution {
-public:
-    bool isPalindrome(string s) {
-        string normalized;
-        for (char c : s) {
-            if (isalnum(c)) {
-                normalized += tolower(c);
+private:
+    bool isPalind(string str){
+        int n=str.size();
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            if(str[left]==str[right]){
+                left++;
+                right--;
+            }
+            else{
+                return false;
             }
         }
-        int left=0;
-        int right=normalized.size()-1;
-        return asd(normalized,left,right);
+        return true;
     }
-    bool asd(string str,int left,int right){
-        if(left>right)return true;
-        if(str[left]!=str[right])return false;
-        return asd(str,left+1,right-1);
+public:
+    bool isPalindrome(string s) {
+        string str;
+        for(auto it:s){
+            if(isalnum(it)){
+                str+=tolower(it);
+            }
+        }
+        return isPalind(str);
     }
 };
