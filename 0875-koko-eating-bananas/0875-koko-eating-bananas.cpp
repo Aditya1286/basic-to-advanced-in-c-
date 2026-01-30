022900748c1e4e2e8d590407d00c052982965e0d
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-01-31 00:10:41 +0530

    Time: 7 ms (71.43%), Space: 39.4 MB (11.82%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,30 +1,31 @@
 class Solution {
-public:
-    long long asd(vector<int> &piles, int k) {
-        long long hours = 0;
-        for (int bananas : piles) {
-            hours += (bananas + k - 1LL) / k;
+private:
+    long long koko(vector<int> nums , int k){
+        long long count=0;
+        for(int kela:nums){
+            int time = (kela+k-1LL)/k;//ceil
+            count+=time;
         }
-        return hours;
+        return count;
     }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high = *max_element(piles.begin(),piles.end());
+        int ans=high;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            long long res=koko(piles,mid);
 
-    int minEatingSpeed(vector<int>& arr, int h) {
-        int low = 1;
-        int high = *max_element(arr.begin(), arr.end());
-        int ans = high;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            long long time = asd(arr, mid);
-
-            if (time <= h) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            if(res<=h){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
+            return ans;
 
-        return ans;
     }
 };
