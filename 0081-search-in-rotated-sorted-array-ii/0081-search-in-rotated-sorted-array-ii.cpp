b017eb1e6520bdcdf2fc25de09d3a655dc60eda2
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-02-02 21:42:42 +0530

    Time: 0 ms (100%), Space: 18 MB (20.78%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -13,7 +13,7 @@ public:
             }
             
             if(arr[low]<=arr[mid]){
-                if(arr[low]<=target && target<arr[mid]){
+                if(arr[low]<=target && target<=arr[mid]){
                     high=mid-1;
                 }
                 else{
@@ -21,7 +21,7 @@ public:
                 }
             }
             else{
-                if(arr[mid]<target && target<=arr[high]){
+                if(arr[mid]<=target && target<=arr[high]){
                     low=mid+1;
                 }
                 else{
