430e84fd37a8d7eefac3ee5164af2b818c4ad190
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-02-26 00:25:03 +0530

    Time: 99 ms (50%), Space: 196.1 MB (19.53%) - LeetHub

---
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        int maxValueSoFar = 0;
+        int ans = 0;
+
+        for (auto &e : events) {
+            int start = e[0];
+            int end = e[1];
+            int value = e[2];
+
+            while (!pq.empty() && pq.top().first < start) {
+                maxValueSoFar = max(maxValueSoFar, pq.top().second);
+                pq.pop();
+            }
+
+            ans = max(ans, maxValueSoFar + value);
+            pq.push({end, value});
+        }
+
+        return ans;
+    }
+};
