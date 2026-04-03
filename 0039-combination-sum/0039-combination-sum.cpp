bbe30d5488834655a1ea8bf13ad4900dda3c89f0
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-04-03 19:14:09 +0530

    Time: 0 ms (100%), Space: 14.1 MB (53.26%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
+private:
+    void dfs(vector<int>& num,int index,int target,vector<int>& subset,vector<vector<int>>& res){
+        if(target==0){
+            res.push_back(subset);
+            return ;
+        }
+        if(index>=num.size() || target<0){
+            return ;
+        }
+        subset.push_back(num[index]);
+        dfs(num,index,target-num[index],subset,res);
+        subset.pop_back();
+        dfs(num,index+1,target,subset,res);
+    }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> combination;
-        dfs(candidates, target, 0, combination, result);
-        return result;
-    }
-
-    void dfs(vector<int>& candidates, int target, int index, vector<int>& combination, vector<vector<int>>& result) {
-        if (target == 0) {
-            result.push_back(combination);
-            return;
-        }
-        if (target < 0 || index == candidates.size()) return;
-        
-        combination.push_back(candidates[index]);
-        dfs(candidates, target - candidates[index], index, combination, result);
-        combination.pop_back();
-        dfs(candidates, target, index + 1, combination, result);
+        vector<vector<int>> res;
+        vector<int> subset;
+        dfs(candidates,0,target,subset,res);
+        return res;
     }
 };
