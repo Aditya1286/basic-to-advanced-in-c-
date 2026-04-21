503623400566f312af2cb0d5fc7ce3ae484e9fcc
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-04-21 20:13:24 +0530

    Time: 298 ms (49.36%), Space: 10.6 MB (67.29%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,41 +1,33 @@
 class Solution {
-public:
-    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    int m, n;
-
-    bool find(vector<vector<char>>& board, int i, int j, int idx, string& word) {
-        if (idx == word.length())
+private:
+    vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    bool asd(vector<vector<char>>& board,int i,int j, string& word, int index){
+        if(index==word.size()){
             return true;
-
-        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '$')
-            return false;
-
-        if (board[i][j] != word[idx])
-            return false;
-
-        char temp = board[i][j];
-        board[i][j] = '$';
-
-        for (auto &dir : directions) {
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
-
-            if (find(board, new_i, new_j, idx + 1, word))
-                return true;
         }
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j]=='$'){
+            return false;
+        }
+        if(board[i][j]!=word[index] )return false;
+        char temp = board[i][j];
+        board[i][j]='$';
+        for(auto &dir:direction){
+            int _x = i+dir[0];
+            int _y = j+dir[1];
+            if(asd(board,_x,_y,word,index+1))return true;
+        }
+        board[i][j]=temp;
 
-        board[i][j] = temp;
         return false;
     }
-
+public:
+    int n,m;
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word[0] && find(board, i, j, 0, word))
-                    return true;
+        n=board.size();
+        m=board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word[0] && asd(board,i,j,word,0))return true;
             }
         }
         return false;
