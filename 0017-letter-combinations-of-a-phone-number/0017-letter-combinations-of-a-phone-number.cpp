646c8f1202e5e35d4e413e6f39d062d3cab94afa
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-05-04 23:52:25 +0530

    Time: 0 ms (100%), Space: 9.7 MB (9.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,42 @@
 class Solution {
-public:
+private:
+
     vector<string> res;
-    void solve(int index,string digit,string temp,unordered_map<char,string> mp){
-        if(index>=digit.length()){
-            res.push_back(temp);
+
+    unordered_map<int, string> mp = {
+        {2, "abc"},
+        {3, "def"},
+        {4, "ghi"},
+        {5, "jkl"},
+        {6, "mno"},
+        {7, "pqrs"},
+        {8, "tuv"},
+        {9, "wxyz"}
+    };
+
+    void bfs(int index, string str, string digits) {
+
+        if (index >= digits.size()) {
+            res.push_back(str);
             return;
         }
-        char ch=digit[index];
-        string str=mp[ch];
-        for(int i=0;i<str.length();i++){
-            temp.push_back(str[i]);
-            solve(index+1,digit,temp,mp);
-            temp.pop_back();
+
+        string ch = mp[digits[index] - '0'];
+
+        for (char c : ch) {
+            bfs(index + 1, str + c, digits);
         }
     }
-    vector<string> letterCombinations(string digit) {
-        if(digit.length()==0){
-            return {};
-        }
-        unordered_map<char,string> mp;
-        mp['2']="abc";
-        mp['3']="def";
-        mp['4']="ghi";
-        mp['5']="jkl";
-        mp['6']="mno";
-        mp['7']="pqrs";
-        mp['8']="tuv";
-        mp['9']="wxyz";
-        string temp="";
-        solve(0,digit,temp,mp);
+
+public:
+
+    vector<string> letterCombinations(string digits) {
+
+        if (digits.empty())
+            return res;
+
+        bfs(0, "", digits);
+
         return res;
     }
 };
