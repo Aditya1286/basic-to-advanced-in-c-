4e40a41ce080e283d416eea94f8154e168dfcb26
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-05-07 00:03:49 +0530

    Time: 233 ms (18.92%), Space: 237.1 MB (19.93%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -2,25 +2,29 @@ class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
         sort(events.begin(), events.end());
-
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-        int maxValueSoFar = 0;
-        int ans = 0;
-
-        for (auto &e : events) {
-            int start = e[0];
-            int end = e[1];
-            int value = e[2];
-
-            while (!pq.empty() && pq.top().first < start) {
-                maxValueSoFar = max(maxValueSoFar, pq.top().second);
-                pq.pop();
+        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
+        return findEvents(events, 0, 0, dp);
+    }
+    int findEvents(vector<vector<int>>& events, int idx, int cnt,
+                   vector<vector<int>>& dp) {
+        if (cnt == 2 || idx >= events.size()) return 0;
+        if (dp[idx][cnt] == -1) {
+            int end = events[idx][1];
+            int lo = idx + 1, hi = events.size() - 1;
+            while (lo < hi) {
+                int mid = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end)
+                    hi = mid;
+                else
+                    lo = mid + 1;
             }
-
-            ans = max(ans, maxValueSoFar + value);
-            pq.push({end, value});
+            int include =
+                events[idx][2] + (lo < events.size() && events[lo][0] > end
+                                      ? findEvents(events, lo, cnt + 1, dp)
+                                      : 0);
+            int exclude = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = max(include, exclude);
         }
-
-        return ans;
+        return dp[idx][cnt];
     }
 };
