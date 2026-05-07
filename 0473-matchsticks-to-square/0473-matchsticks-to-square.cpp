910d0c83d14bcdbdbfded000db4f9c0695106121
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-05-07 14:06:52 +0530

    Time: 161 ms (46.8%), Space: 74 MB (5.01%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -1,32 +1,26 @@
 class Solution {
-public:
-    bool makesquare(vector<int>& matchsticks) {
-        int totalLength = accumulate(matchsticks.begin(), matchsticks.end(), 0);
-        if (totalLength % 4 != 0) return false;
-
-        int length = totalLength / 4;
-        vector<int> sides(4, 0);
-        sort(matchsticks.rbegin(), matchsticks.rend());
-
-        return dfs(matchsticks, sides, 0, length);
-    }
-
 private:
-    bool dfs(vector<int>& matchsticks, vector<int>& sides, int index, int length) {
-        if (index == matchsticks.size()) {
+    bool dfs(vector<int> arr,int length, vector<int>& sides, int index){
+        if(index==arr.size()){
             return true;
         }
-
-        for (int i = 0; i < 4; i++) {
-            if (sides[i] + matchsticks[index] <= length) {
-                sides[i] += matchsticks[index];
-                if (dfs(matchsticks, sides, index + 1, length)) return true;
-                sides[i] -= matchsticks[index];
+        for(int i=0;i<4;i++){
+            if(sides[i]+arr[index]<=length){
+                sides[i]+=arr[index];
+                if(dfs(arr,length,sides,index+1))return true;
+                sides[i]-=arr[index];
             }
-
-            if (sides[i] == 0) break;
+            if(sides[i]==0)break;
         }
-
         return false;
+    }
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        int sum = accumulate(matchsticks.begin(),matchsticks.end(),0);
+        if(sum%4!=0)return false;
+        int length=sum/4;
+        vector<int> sides(4,0);
+        sort(matchsticks.rbegin(),matchsticks.rend());
+        return dfs(matchsticks,length,sides,0);
     }
 };
