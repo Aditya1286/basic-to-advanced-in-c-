4a09a7600eb989c46308564ac625f4b5c957de33
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-05-12 16:17:50 +0530

    Time: 43 ms (5.8%), Space: 50.1 MB (5.01%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,51 +1,37 @@
 class Solution {
-public:
-    bool isSafe1(int row, int col, vector<string> board, int n) {
-        int duprow = row;
-        int dupcol = col;
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            row--;
-            col--;
+private:
+    bool isSafe(int r,int c,vector<string> board){
+
+        for(int i=r-1;i>=0;i--){
+            if(board[i][c]=='Q')return false;
         }
-        col = dupcol;
-        row = duprow;
-        while (col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            col--;
+        for(int i=r-1,j=c-1;i>=0&&j>=0;i--,j--){
+            if(board[i][j]=='Q')return false;
         }
-        row = duprow;
-        col = dupcol;
-        while (row < n && col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            row++;
-            col--;
+        for(int i=r-1,j=c+1;i>=0&&j<board.size();i--,j++){
+            if(board[i][j]=='Q')return false;
         }
         return true;
-    }
 
-    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n) {
-        if (col == n) {
-            ans.push_back(board);
+    }
+    void backtrack(int r,vector<string>& board){
+        if(r==board.size()){
+            res.push_back(board);
             return;
         }
-        for (int row = 0; row < n; row++) {
-            if (isSafe1(row, col, board, n)) {
-                board[row][col] = 'Q';
-                solve(col + 1, board, ans, n);
-                board[row][col] = '.';
+        for(int c=0;c<board.size();c++){
+            if(isSafe(r,c,board)){
+                board[r][c]='Q';
+                backtrack(r+1,board);
+                board[r][c]='.';
             }
         }
     }
-
+public:
+    vector<vector<string>> res;
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n, '.');
-        for (int i = 0; i < n; i++) {
-            board[i] = s;
-        }
-        solve(0, board, ans, n);
-        return ans;
+        vector<string> board(n,string(n,'.'));
+        backtrack(0,board);
+        return res;
     }
 };
