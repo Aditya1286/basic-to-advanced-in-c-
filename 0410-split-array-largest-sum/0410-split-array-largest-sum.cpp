821d5935999de20c25f5649403570c177a6f8486
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-06-25 14:56:48 +0530

    Time: 0 ms (100%), Space: 10.6 MB (41.95%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,34 +1,34 @@
 class Solution {
-public:
-    int asd(vector<int>arr,int n){
-        int painter=1;
-        int nowork=0;
+private:
+    int asd(vector<int>&arr,int target){
+        int count=1;
+        int tar=0;
         for(int i=0;i<arr.size();i++){
-            if(nowork+arr[i]<=n){
-                nowork+=arr[i];
+            if(tar+arr[i]<=target){
+                tar+=arr[i];
             }
             else{
-                painter++;
-                nowork=arr[i];
+                count++;
+                tar=arr[i];
             }
         }
-        return painter;
+        return count;
     }
-    int splitArray(vector<int>& arr, int k) {
-        int low=0;
-        int high=0;
-        int n=arr.size();
-        for(int i=0;i<n;i++){
-            low=max(low,arr[i]);
-            high+=arr[i];
-        }
-        if(k>n)return -1;
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
         while(low<=high){
-            int mid=(low+high)/2;
-            int count=asd(arr,mid);
-            if(count>k)low=mid+1;
-            else high=mid-1;
+            int mid=high-(high-low)/2;
+            int x=asd(nums,mid);
+            if(x>k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         return low;
+
     }
 };
