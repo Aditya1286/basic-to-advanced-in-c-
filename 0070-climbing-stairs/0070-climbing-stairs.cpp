bf04561aa94a6bc6eb106ca5f8601e967ea0a65f
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-07-21 14:33:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        if(n<=2)return n;
+        vector<int> stairs(n+1);
+        stairs[1]=1;
+        stairs[2]=2;
+        for(int i=3;i<=n;i++){
+            int a=stairs[i-2];
+            int b=stairs[i-1];
+            int c=a+b;
+            stairs[i]=c;
+        }
+        return stairs[n];
+
     }
 };
