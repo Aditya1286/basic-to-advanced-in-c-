5ca3f14d27a50bdba5960596afc024d09f783fea
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-07-22 14:27:56 +0530

    Time: 22 ms (14.14%), Space: 105.6 MB (18.97%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    int dfs(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& visited){
+
+    int row=grid.size();
+    int col=grid[0].size();
+
+    if(i<0 || j<0 || i>=row || j>=col)
+        return 1;
+
+    if(grid[i][j]==0)
+        return 1;
+
+    if(visited[i][j])
+        return 0;
+
+    visited[i][j]=1;
+
+    return dfs(i,j+1,grid,visited)
+         + dfs(i+1,j,grid,visited)
+         + dfs(i-1,j,grid,visited)
+         + dfs(i,j-1,grid,visited);
+}
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int row=grid.size();
+        int col=grid[0].size();
+        vector<vector<int>> visited(row,vector<int>(col,0));
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]==1 && visited[i][j]==0){
+                    return dfs(i,j,grid,visited);
+                }
+            }
+        }
+        return -1;
+    }
+};
