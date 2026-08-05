7b69d1cd5558881a4f803ebf106ba1489544d0f9
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2026-08-05 21:27:57 +0530

    Time: 455 ms (23.45%), Space: 373.1 MB (16.21%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    void dfs(int node,vector<int>& visited,vector<vector<int>>& adj){
+        visited[node]=1;
+        for(auto it:adj[node]){
+            if(!visited[it]){
+                dfs(it,visited,adj);
+            }
+        }
+    }
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+        for(auto it:invocations){
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+        }
+        vector<int> visited(n,0);
+        dfs(k,visited,adj);
+        for(auto it:invocations){
+            int u=it[0];
+            int v=it[1];
+            if(visited[u]==0 && visited[v]==1){
+                vector<int> ans;
+                for(int i=0;i<n;i++){
+                    ans.push_back(i);
+                }
+                return ans;
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
